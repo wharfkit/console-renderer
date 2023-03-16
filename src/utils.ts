97befe845867c81aca330f0fe37a5be8f493f1c2
dafuga <danielfugere28@gmail.com>
2023-03-16 18:17:57 -0400

    cleanup: code cleanup

--- src/utils.ts
+++ src/utils.ts
@@ -4,11 +4,6 @@ export function countdown(expirationTimeString?: string, interval = 10000) {
         : Date.now() + 120000
     const startTime = Date.now()
     const remainingTime = expirationTime - startTime
-    const remainingSeconds = Math.ceil(remainingTime) / 1000
-
-    const minutes = Math.floor(remainingSeconds / 60)
-    const seconds = remainingSeconds % 60
-    console.log(`\nTime remaining: ${minutes}:${seconds.toString().padStart(2, '0')}`)
 
     const intervalId = setInterval(() => {
         const elapsedTime = Date.now() - startTime
